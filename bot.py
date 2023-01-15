400a49d4385b89f5e3b04ba98adaccff6f0632e9
MiguelX413 <lbpgaming27@gmail.com>
2023-01-15 13:03:16 -0800

    Build basic bot

--- bot.py
+++ bot.py
@@ -1,10 +1,37 @@
 #!/usr/bin/env python3
-def bot() -> None:
-    pass
+import logging
+import os
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("I'm a bot, please talk to me!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
+
+def bot(
+    token: str,
+) -> None:
+    application = ApplicationBuilder().token(token).build()
+
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+
+    application.add_handler(echo_handler)
+
+    application.run_polling()
 
 
 def main() -> None:
-    pass
+    token = os.getenv("token")
+    if not isinstance(token, str):
+        raise TypeError("No Token")
+    logging.basicConfig(level=logging.INFO)
+    bot(token)
 
 
 if __name__ == "__main__":
